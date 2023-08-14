691733774efa587880d836ba7e9b6845bc35400f
Rubens Brandao <git@rubens.io>
2023-08-14 08:48:30 -0300

    Use icicle-vm buider for SleighData
    There is no need to bundle the sleigh files from ghidra and parse that
    manually, icicle-vm already does that in the function `build_sleigh_for`.
    It's only required to set the environment variable `GHIDRA_SRC` to the root
    of ghidra repository.
    
    This also changes the input/output varnodes from HashSet to Vec, because
    some instructions could use the same register as input/output multiple
    times, eg `ADD EAX, EAX, EAX`.

--- icicle-disasm/src/lib.rs
+++ icicle-disasm/src/lib.rs
@@ -1,65 +1,10 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-
 use pcode::VarNode;
-use sleigh_runtime::Decoder;
-use sleigh_runtime::Lifter;
-use sleigh_runtime::SleighData;
-
-use sleigh_parse::{ast, Input, Parser};
-
-#[derive(Debug, serde::Deserialize)]
-struct Set {
-    pub name: String,
-    pub val: u64,
-}
-
-#[allow(unused)]
-#[derive(Debug, serde::Deserialize)]
-struct ProgramCounter {
-    register: String,
-}
-
-#[allow(unused)]
-#[derive(Debug, serde::Deserialize)]
-struct ContextSet {
-    pub space: String,
-    #[serde(rename = "$value")]
-    pub set: Vec<Set>,
-}
-
-#[allow(unused)]
-#[derive(Debug, serde::Deserialize)]
-struct ContextData {
-    context_set: ContextSet,
-    #[serde(skip)]
-    tracked_set: Vec<()>,
-}
-
-/// A SLEIGH processor specification file
-#[derive(Debug, serde::Deserialize)]
-struct PSpec {
-    #[allow(unused)]
-    #[serde(skip)]
-    properties: Vec<()>,
-    #[allow(unused)]
-    programcounter: ProgramCounter,
-    context_data: ContextData,
-    #[allow(unused)]
-    #[serde(skip)]
-    register_data: Vec<()>,
-}
-extern crate embeddir;
-
-use once_cell::sync::Lazy;
-
-const DIR: Lazy<&HashMap<&str, &[u8]>> =
-    Lazy::new(|| Box::leak(Box::new(embeddir::embed!("languages"))));
+use sleigh_runtime::{Decoder, Lifter, SleighData};
 
 pub struct Insn {
     pub string: String,
-    pub inputs: HashSet<VarNode>,
-    pub outputs: HashSet<VarNode>,
+    pub inputs: Vec<VarNode>,
+    pub outputs: Vec<VarNode>,
     pub insn: sleigh_runtime::Instruction,
     pub pcode: Vec<pcode::Instruction>,
     pub load: bool,
@@ -73,60 +18,33 @@ pub struct Disassembler {
 
 impl Disassembler {
     pub fn new() -> Self {
-        struct EmbeddedInput<'a> {
-            files: &'a HashMap<&'a str, &'a [u8]>,
-        }
-        let input = EmbeddedInput { files: &DIR };
-        impl<'a> Input for EmbeddedInput<'a> {
-            fn open(&mut self, name: &str) -> std::io::Result<String> {
-                Ok(std::str::from_utf8(self.files[name]).unwrap().to_owned())
-            }
-        }
-        let mut parser = Parser::new(input);
-        parser.include_file("x86-64.slaspec");
+        // parse the sleigh file into this SleighData and the default context
+        // for this architecture
+        let (sleigh, default_context) =
+            icicle_vm::build_sleigh_for("x86_64".parse().unwrap()).unwrap();
 
-        //let sleigh: SleighData = sleigh_compile::from_path("/Users/jrmuizel/src/ghidra/Ghidra/Processors/x86/data/languages/x86-64.slaspec").unwrap();
-        let sleigh: SleighData = sleigh_compile::build_inner(parser, false).unwrap();
-
-        //let ctx = &mut config.context;
-
-        //if let Some(pspec_path) = config.processor_spec_path {
-        let pspec: PSpec = serde_xml_rs::from_reader(DIR["x86-64.pspec"]).unwrap();
-        //.map_err(|e| BuildError::FailedToParsePspec(e.to_string()))?;
-
-        let mut initial_ctx = 0_u64;
-        for entry in &pspec.context_data.context_set.set {
-            let field = sleigh.get_context_field(&entry.name).unwrap();
-
-            field.field.set(&mut initial_ctx, entry.val as i64);
-        }
-        //ctx.push(initial_ctx);
-        //}
         let mut decoder = Decoder::new();
-        decoder.global_context = initial_ctx;
+        decoder.global_context = default_context;
 
-        for r in &sleigh.named_registers {
+        //for r in &sleigh.named_registers {
+        //    dbg!(r.var, sleigh.get_str(r.name));
+        //}
 
-            //dbg!(r.var, sleigh.get_str(r.name));
-        }
-
-        let mut lifter = sleigh_runtime::Lifter::new();
+        let lifter = sleigh_runtime::Lifter::new();
         Disassembler { sleigh, decoder, lifter }
     }
 
     pub fn decode(&mut self, base_addr: u64, bytes: &[u8]) -> Insn {
-        let mut d = self;
-
-        //d.decoder.set_inst(0, &[0x4c, 0x85, 0x7d, 0x30]);
+        //self.decoder.set_inst(0, &[0x4c, 0x85, 0x7d, 0x30]);
         //decoder.set_inst(0, &[0x90]);
-        //d.decoder.set_inst(0, &[0x49, 0x89, 0xe5]);
-        //d.decoder.set_inst(0, &[0x55]);
-        d.decoder.set_inst(base_addr, bytes);
-        let insn = d.sleigh.decode(&mut d.decoder).unwrap();
+        //self.decoder.set_inst(0, &[0x49, 0x89, 0xe5]);
+        //self.decoder.set_inst(0, &[0x55]);
+        self.decoder.set_inst(base_addr, bytes);
+        let insn = self.sleigh.decode(&mut self.decoder).unwrap();
 
-        let str = d.sleigh.disasm(&insn);
-        let pcode = d.lifter.lift(&d.sleigh, &insn).unwrap();
-        let mut input_regs = HashSet::new();
+        let str = self.sleigh.disasm(&insn);
+        let pcode = self.lifter.lift(&self.sleigh, &insn).unwrap();
+        let mut input_regs = Vec::new();
         let mut output_regs = Vec::new();
         let mut load = false;
         let mut store = false;
@@ -140,11 +58,11 @@ impl Disassembler {
             //dbg!(insn.inputs.first());
             match insn.inputs.first() {
                 pcode::Value::Var(var) => {
-                    for r in &d.sleigh.named_registers {
+                    for r in &self.sleigh.named_registers {
                         if var == r.var {
-                            //println!("{}", d.sleigh.get_str(r.name));
-                            //input_regs.insert(d.sleigh.get_str(r.name).to_owned());
-                            input_regs.insert(var);
+                            //println!("{}", self.sleigh.get_str(r.name));
+                            //input_regs.insert(self.sleigh.get_str(r.name).to_owned());
+                            input_regs.push(var);
                         }
                     }
                 }
@@ -153,21 +71,21 @@ impl Disassembler {
             //dbg!(insn.inputs.second());
             match insn.inputs.second() {
                 pcode::Value::Var(var) => {
-                    for r in &d.sleigh.named_registers {
+                    for r in &self.sleigh.named_registers {
                         if var == r.var {
-                            //println!("{}", d.sleigh.get_str(r.name));
-                            //input_regs.insert(d.sleigh.get_str(r.name).to_owned());
-                            input_regs.insert(var);
+                            //println!("{}", self.sleigh.get_str(r.name));
+                            //input_regs.insert(self.sleigh.get_str(r.name).to_owned());
+                            input_regs.push(var);
                         }
                     }
                 }
                 pcode::Value::Const(_, _) => {}
             }
             //dbg!(insn.output);
-            for r in &d.sleigh.named_registers {
+            for r in &self.sleigh.named_registers {
                 if insn.output == r.var {
-                    //println!("{}", d.sleigh.get_str(r.name));
-                    //output_regs.push(d.sleigh.get_str(r.name).to_owned());
+                    //println!("{}", self.sleigh.get_str(r.name));
+                    //output_regs.push(self.sleigh.get_str(r.name).to_owned());
                     output_regs.push(insn.output)
                 }
             }
@@ -175,8 +93,8 @@ impl Disassembler {
 
         Insn {
             string: str.unwrap(),
-            inputs: input_regs.to_owned(),
-            outputs: output_regs.into_iter().collect(),
+            inputs: input_regs,
+            outputs: output_regs,
             insn,
             pcode: pcode.instructions.clone(),
             load,
